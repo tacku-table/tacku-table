534f652985ccc41fd508de5b56b7236b474a0ede
harenohee <panda4859@gmail.com>
2023-02-08 17:48:02 +0900

    feat: firebase 연결 create
    제목, 내용 db 저장
    
    #1 by @harenohee

--- pages/communityPage.tsx
+++ pages/communityPage.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { useState } from "react";
+import {
+  onSnapshot,
+  query,
+  collection,
+  doc,
+  orderBy,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
-const communityPage = () => {
-  return <div></div>;
+import { dbService } from "@/firebase";
+const CommunityPage = () => {
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  const newPost = {
+    // 유저 아이디 아직 안넣음
+    title,
+    content,
+    writtenDate: Date.now(),
+  };
+
+  const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await addDoc(collection(dbService, "communityPost"), newPost);
+    setTitle("");
+    setContent("");
+  };
+
+  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleChangeContent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(event.target.value);
+  };
+
+  return (
+    <div className="px-4 py-8 bg-slate-400">
+      <span>글쓰기</span>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          onChange={handleChangeTitle}
+          value={title}
+          required
+        ></input>
+        <input
+          type="text"
+          onChange={handleChangeContent}
+          value={content}
+          required
+        ></input>
+        <button type="submit">등록</button>
+      </form>
+    </div>
+  );
 };
 
-export default communityPage;
+export default CommunityPage;
