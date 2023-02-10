bc038a3e7451434c3dfc26d68be0aaab16571acb
harenohee <panda4859@gmail.com>
2023-02-10 10:38:51 +0900

    feat: 텍스트 에디터 컴포넌트화
    document 에러 발생 안함
    
    #4 by @harenohee

--- components/TextEditor.jsx
+++ components/TextEditor.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
 
-const TextEditor = () => {
-  return <div>TextEditor</div>;
+const TextEditor = ({ content, setContent }) => {
+  const quillRef = useRef();
+  const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  });
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { header: "3" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  return (
+    <QuillNoSSRWrapper
+      forwardedRef={quillRef}
+      modules={modules}
+      value={content}
+      placeholder="본문"
+      onChange={setContent}
+      theme="snow"
+    />
+  );
 };
 
 export default TextEditor;
