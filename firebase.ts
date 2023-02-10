d6dd16de6c64832f46ca478a6a229a7065a4d992
hongdakyung <bongbk72@naver.com>
2023-02-10 16:18:41 +0900

    Feat : 로그인 로직 추가 및 loginPage에 회원가입,비밀번호 찾기 이동 추가
    - 로그인 로직 추가하였습니다
    - 로그인 유효성 검사 로직 추가하였습니다
    - 회원가입, 비밀번호 찾기 버튼 이동시 해당 컴포넌트가 리턴되도록 코드 추가하였습니다
    
    by @ddoqi

--- firebase.ts
+++ firebase.ts
@@ -3,17 +3,38 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+//다경 설정으로 되어있어요
 const firebaseConfig = {
-    apiKey: "AIzaSyCs3OtP4bwHc4DzziKLIzM4vlJMhyuflEg",
-    authDomain: "reactnative-dbdad.firebaseapp.com",
-    databaseURL: "https://reactnative-dbdad-default-rtdb.firebaseio.com",
-    projectId: "reactnative-dbdad",
-    storageBucket: "reactnative-dbdad.appspot.com",
-    messagingSenderId: "103307494325",
-    appId: "1:103307494325:web:cb3f1b74c60ccc2bba8ffc",
+  apiKey: "AIzaSyC1Z5Sl0P_WuafG-4Fgx08TeKD1KzSAjFU",
+  authDomain: "tacku-table.firebaseapp.com",
+  projectId: "tacku-table",
+  storageBucket: "tacku-table.appspot.com",
+  messagingSenderId: "470777376989",
+  appId: "1:470777376989:web:ed93c811f68a6dd2ee2b34",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const dbService = getFirestore(app);
 export const authService = getAuth(app);
 export const storage = getStorage(app);
+
+// 채하님꺼
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCs3OtP4bwHc4DzziKLIzM4vlJMhyuflEg",
+//     authDomain: "reactnative-dbdad.firebaseapp.com",
+//     databaseURL: "https://reactnative-dbdad-default-rtdb.firebaseio.com",
+//     projectId: "reactnative-dbdad",
+//     storageBucket: "reactnative-dbdad.appspot.com",
+//     messagingSenderId: "103307494325",
+//     appId: "1:103307494325:web:cb3f1b74c60ccc2bba8ffc",
+// };
+
+// 다경님꺼
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC1Z5Sl0P_WuafG-4Fgx08TeKD1KzSAjFU",
+//     authDomain: "tacku-table.firebaseapp.com",
+//     projectId: "tacku-table",
+//     storageBucket: "tacku-table.appspot.com",
+//     messagingSenderId: "470777376989",
+//     appId: "1:470777376989:web:ed93c811f68a6dd2ee2b34",
+//   };
