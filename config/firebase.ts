001448ce44725a80b0ad3f1d6a658c4ff0946e30
harenohee <panda4859@gmail.com>
2023-02-15 15:27:01 +0900

    fix: 파베 수정
    터져서 새로 팠습니다.
    
    by @harenohee

--- config/firebase.ts
+++ config/firebase.ts
@@ -22,34 +22,33 @@ import { getStorage } from "firebase/storage";
 //     appId: "1:139053130279:web:ae70760c52da59bb6b2402",
 // };
 
-// 다경님꺼
+// 다경님꺼2
 // const firebaseConfig = {
-//     apiKey: "AIzaSyDKVOAQVuHCId2KTKdPFbzNvLpp8TXrLsU",
-//     authDomain: "tacku-table-private.firebaseapp.com",
-//     projectId: "tacku-table-private",
-//     storageBucket: "tacku-table-private.appspot.com",
-//     messagingSenderId: "351669114811",
-//     appId: "1:351669114811:web:769e3f58dc6fd33b9d169e",
+//   apiKey: "AIzaSyB_o07bi777IxMsKkBeNfOKZFHy-nKJ1gc",
+//   authDomain: "testtest-9c70b.firebaseapp.com",
+//   projectId: "testtest-9c70b",
+//   storageBucket: "testtest-9c70b.appspot.com",
+//   messagingSenderId: "472171635233",
+//   appId: "1:472171635233:web:b539b40ca52e9b52d23960",
 // };
 
-// 다경님꺼2
-const firebaseConfig = {
-  apiKey: "AIzaSyB_o07bi777IxMsKkBeNfOKZFHy-nKJ1gc",
-  authDomain: "testtest-9c70b.firebaseapp.com",
-  projectId: "testtest-9c70b",
-  storageBucket: "testtest-9c70b.appspot.com",
-  messagingSenderId: "472171635233",
-  appId: "1:472171635233:web:b539b40ca52e9b52d23960",
-};
-
 // 희진
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDKVOAQVuHCId2KTKdPFbzNvLpp8TXrLsU",
+//   authDomain: "tacku-table-private.firebaseapp.com",
+//   projectId: "tacku-table-private",
+//   storageBucket: "tacku-table-private.appspot.com",
+//   messagingSenderId: "351669114811",
+//   appId: "1:351669114811:web:769e3f58dc6fd33b9d169e",
+// };
+// 희진2
 const firebaseConfig = {
-  apiKey: "AIzaSyDKVOAQVuHCId2KTKdPFbzNvLpp8TXrLsU",
-  authDomain: "tacku-table-private.firebaseapp.com",
-  projectId: "tacku-table-private",
-  storageBucket: "tacku-table-private.appspot.com",
-  messagingSenderId: "351669114811",
-  appId: "1:351669114811:web:769e3f58dc6fd33b9d169e",
+  apiKey: "AIzaSyAy8yNJxxqNMMmKZldPnjX-cbmrLhg49Yw",
+  authDomain: "tacku-table-private2.firebaseapp.com",
+  projectId: "tacku-table-private2",
+  storageBucket: "tacku-table-private2.appspot.com",
+  messagingSenderId: "257373963092",
+  appId: "1:257373963092:web:9e669c8be252d7b6f47ec1",
 };
 
 // Initialize Firebase
