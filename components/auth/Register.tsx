fd1fd9d5b48ffcc8ab39ec2609739e7137eb9f60
harenohee <panda4859@gmail.com>
2023-02-16 12:02:21 +0900

    feat: 회원가입 user 컬렉션 추가합니다.
    마이페이지에서 조회할 컬렉션입니다.
    
    by @harenohee

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
-import { authService } from "@/config/firebase";
+import { authService, dbService } from "@/config/firebase";
 import {
   createUserWithEmailAndPassword,
   signOut,
   updateProfile,
 } from "firebase/auth";
 import { emailRegex, pwRegex } from "@/util";
+import { setDoc, doc, addDoc, collection } from "firebase/firestore";
 
 const RegisterPage = () => {
   // useRef로 취득하는 DOM은 최초 mount되기 전엔 null이다
@@ -59,8 +60,16 @@ const RegisterPage = () => {
       return;
     }
     createUserWithEmailAndPassword(authService, email, pw)
-      .then((data) => {
-        updateProfile(data.user, {
+      .then(async (data) => {
+        console.log("회원 데이터", data.user.uid);
+        // const docRef = data.user.uid;
+        await setDoc(doc(dbService, "user", data.user.uid), {
+          userId: data.user.uid,
+          userNickname: nickname,
+          userEmail: email,
+          userPw: pw,
+        });
+        await updateProfile(data.user, {
           displayName: nickname,
         });
         alert("회원가입성공! 로그인해주세요!");
