018bc8d5b8b3e38ba7341d1eede0c1f883f9c5ae
harenohee <panda4859@gmail.com>
2023-02-16 11:24:24 +0900

    docs: image 외부 url
    url 불러오기 관련 코드 추가
    
    by @harenohee

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-        styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
+  },
 };
 
 module.exports = nextConfig;
