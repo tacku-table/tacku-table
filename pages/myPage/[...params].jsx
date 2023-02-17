029e4fcaea0608971973547300ce938e5d0dd32e
harenohee <panda4859@gmail.com>
2023-02-17 15:31:25 +0900

    fix: 스토리지 파일 유형 image로 수정
    프로필사진 업로드 후파일유형: application/octet-stream.
    추후 번거로울까봐 다른 이미지들과 같은 파일유형으로 맞춰주었습니다.
    
    #8 by @harenohee

--- pages/myPage/[...params].jsx
+++ pages/myPage/[...params].jsx
@@ -1,14 +1,8 @@
 import { authService, dbService } from "../../config/firebase";
 // import { async } from "@firebase/util";
-import { updateProfile } from "firebase/auth";
+import { updatePassword, updateProfile } from "firebase/auth";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import {
-  getDownloadURL,
-  ref,
-  listAll,
-  uploadBytes,
-  uploadString,
-} from "firebase/storage";
+import { getDownloadURL, ref, listAll, uploadBytes } from "firebase/storage";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
@@ -16,48 +10,88 @@ import profile from "../../public/images/profile.jpeg";
 import { storage } from "../../config/firebase";
 const ProfileEdit = () => {
   const [userInfo, setUserInfo] = useState({});
+  const [uid, setUid] = useState("");
+  // 프로필이미지 변경
   const [photoImgURL, setPhotoImgURL] = useState();
   const [imageUpload, setImageUpload] = useState(null);
   const [isEditImg, setIsEditImg] = useState(false);
+  const [imgPreview, setImgPreview] = useState();
+  // 비밀번호 변경
+  const [changeUserPw, setChangeUserPw] = useState("");
+  // 비밀번호 확인
+  const [confirmChangeUserPw, setConfirmChangeUserPw] = useState("");
+  // 닉네임 변경
+  const [changeUserNickname, setChangeUserNickname] = useState("");
 
   const router = useRouter();
+
+  const currentUser = authService?.currentUser;
+  // console.log(currentUser);
+
+  useEffect(() => {
+    // const uid = router.query.id;
+    const sessionStorageUser = sessionStorage.getItem("User");
+    const parsingUser = JSON.parse(sessionStorageUser);
+    setUid(parsingUser?.uid);
+    // console.log("uid", uid);
+    // getCurrentUserInfo(uid);
+    // getUserProfileImg();
+  }, []);
   const getCurrentUserInfo = async (currentUserUid) => {
     await getDoc(doc(dbService, "user", currentUserUid)).then((doc) => {
       setUserInfo(doc.data());
     });
   };
-  useEffect(() => {
-    const id = router.query.id;
-    getCurrentUserInfo(id);
-    getUserProfileImg();
-  });
 
   const handleImageFile = (event) => {
-    setImageUpload(event.target.files?.[0]);
+    const file = event.target.files?.[0];
+    setImageUpload(file);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      // console.log("파일 정상적으로 불러옴");
+      const selectedImgUrl = reader.result;
+      console.log("selectedImgUrl", selectedImgUrl);
+      setImgPreview(selectedImgUrl);
+    };
+    // setImageUpload(event.target.files?.[0]);
   };
-  // div를 클릭해도 input이 클릭되도록 하기
-  const fileRef = useRef(null);
-  const onClickUpload = () => {
-    fileRef.current?.click();
+  // 인풋값 관리 함수
+  const inputChangeSetFunc = (event, setFunction) => {
+    setFunction(event.target.value);
   };
-  // 프로필 닉네임 변경 함수
-  const handleUpdateDisplayName = async () => {};
 
-  const handleUpdateProfile = async () => {
+  // div를 클릭해도 input이 클릭되도록 하기
+  // const fileRef = useRef(null);
+  // const onClickUpload = () => {
+  //   fileRef.current?.click();
+  // };
+
+  // 닉네임, 비밀번호, 이미지 같이 업로드
+  const handleUpdateProfile = async (uid) => {
+    const docRef = doc(dbService, "user", uid);
     // if (imageUpload === null) return;
-    const imageRef = ref(storage, "profileImage/" + userInfo.userId);
+    const imageRef = ref(storage, `profileImage/${uid}`);
     await uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then(async (url) => {
-        await updateProfile(authService?.currentUser, {
+      getDownloadURL(snapshot.ref).then((url) => {
+        // updatePassword(currentUser, changeUserPw)
+        //   .then(() => console.log("비밀번호 변경 완료!"))
+        //   .catch((error) => console.log("비밀번호 변경 에러: ", error));
+        updateProfile(currentUser, {
+          displayName: changeUserNickname,
           photoURL: url,
-        });
-        await updateDoc(doc(dbService, "user", userInfo?.userId), {
+        })
+          .then(() => console.log("닉네임 변경 완료!"))
+          .catch((error) => console.log("닉네임 변경 에러: ", error));
+        updateDoc(docRef, {
           userImg: url,
+          userNickname: changeUserNickname,
+          userPw: changeUserPw,
         }).then(() => {
           alert("프로필이미지 업로드 성공");
-          setPhotoImgURL(url);
           console.log(url);
         });
+        setImgPreview(url);
         // setPhotoURL((prev) => [...prev, url]);
       });
     });
@@ -79,12 +113,54 @@ const ProfileEdit = () => {
 
   return (
     <>
-      <div className="flex justify-between">
+      <div>
+        <form
+          className="flex flex-col"
+          onSubmit={() => handleUpdateProfile(uid)}
+        >
+          <input
+            id="picture"
+            type="file"
+            accept="image/*"
+            onChange={handleImageFile}
+          />
+          <img src={imgPreview} width={100} height={100} alt="프리뷰" />
+          {/* <label>
+            비밀번호 변경:
+            <input
+              type="text"
+              onChange={(event) => inputChangeSetFunc(event, setChangeUserPw)}
+              className="w-[256px] border border-black"
+            />
+          </label>
+          <label>
+            비밀번호 확인:
+            <input
+              type="text"
+              onChange={(event) =>
+                inputChangeSetFunc(event, setConfirmChangeUserPw)
+              }
+              className="w-[256px] border border-black"
+            />
+          </label> */}
+          <label>
+            닉네임 변경:
+            <input
+              type="text"
+              onChange={(event) =>
+                inputChangeSetFunc(event, setChangeUserNickname)
+              }
+              className="w-[256px] border border-black"
+            />
+          </label>
+
+          <input type="submit" value="수정하기" />
+        </form>
         <div
           className="hover:opacity-60"
-          onClick={() => setIsEditImg(!isEditImg)}
+          // onClick={() => setIsEditImg(!isEditImg)}
         >
-          <label>
+          {/* <label>
             {isEditImg ? (
               <input
                 id="picture"
@@ -101,10 +177,9 @@ const ProfileEdit = () => {
                 height={100}
                 alt="변경된 이미지"
               />
-            )}
-          </label>
+            )} */}
+          {/* </label> */}
         </div>
-        <button onClick={handleUpdateProfile}>업로드</button>
       </div>
     </>
   );
