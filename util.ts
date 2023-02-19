e9c7b7c70b4b0f9fcdc8a0377e0e14677492a139
harenohee <panda4859@gmail.com>
2023-02-19 16:28:09 +0900

    feat: 닉네임 유효성 검사 추가
    마이페이지에서 닉네임 변경 시 쓸 로직 추가
    - 2~8자
    - 영어, 숫자, 한글 입력 (별도 가능)
    - 한글 초성 입력 불가
    
    by @harenohee

--- util.ts
+++ util.ts
@@ -1,3 +1,4 @@
 export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const pwRegex =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+export const nickRegex = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$/;
