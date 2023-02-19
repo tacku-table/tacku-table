1628a168cadc5d44d4be9baafe573678d48ace7b
harenohee <panda4859@gmail.com>
2023-02-19 12:44:32 +0900

    docs: 이미지 관련 코드 삭제
    Image태그 속성인 loader로도 잘 불러와지는 것 확인하여 해당 코드는 삭제합니다.
    
    by @harenohee

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
