cfe5b44fc0d99afc8e3486948bd845a67a896e0e
JH-anfseo <51734656+JH-anfseo@users.noreply.github.com>
2023-02-20 14:28:46 +0900

    fix : 북마크 갯수 삭제, 북마크 uid만 들어가도록 수정

--- components/detail/Bookmark.tsx
+++ components/detail/Bookmark.tsx
@@ -13,11 +13,12 @@ import { authService, dbService } from "@/config/firebase";
 const Bookmark = (props: any) => {
   //북마크
   const [bookMark, setBookMark] = useState<any[]>([]);
-  const [countBookMark, setCountBookMark] = useState<any>();
+  const [countBookMark, setCountBookMark] = useState<any>("");
   const [toggleBookmark, setToggleBookmark] = useState<boolean>(false);
   //현재 로그인된 유저
   const currentUser: any = authService.currentUser?.uid;
   //유저 북마크 모아오기
+
   useEffect(() => {
     const bookmarkLoad = async () => {
       try {
@@ -31,12 +32,20 @@ const Bookmark = (props: any) => {
     };
     bookmarkLoad();
   }, [dbService, props.postId]);
-  //recipe에 유저 id 추가
-  // useEffect(() => {
-  //   onSnapshot(doc(dbService, "recipe", props.postId), (snapshot) => {
-  //     setCountBookMark(snapshot.data());
-  //   });
-  // }, [dbService, props.postId]);
+  useEffect(() => {
+    onSnapshot(doc(dbService, "recipe", props.postId), (snapshot) => {
+      setCountBookMark(snapshot.data());
+    });
+  }, [dbService, props.postId]);
+
+  // localStorage불러오기
+  useEffect(() => {
+    let storedVisits = window.localStorage.getItem("toggleBookmark");
+    if (storedVisits !== null) {
+      const parsingtoggle = JSON.parse(storedVisits);
+      setToggleBookmark(!parsingtoggle);
+    }
+  }, []);
 
   //북마크 토글
   useEffect(() => {
@@ -44,57 +53,40 @@ const Bookmark = (props: any) => {
       bookMark.findIndex((mark) => mark.id === props.postId) !== -1
     );
   }, [bookMark, props.postId]);
+
   //북마크 db 추가 삭제
   const bookMarkPost = async () => {
+    //localStorage에 저장
+    window.localStorage.setItem(
+      "toggleBookmark",
+      JSON.stringify(toggleBookmark)
+    );
+
     if (toggleBookmark) {
       setToggleBookmark(!toggleBookmark);
-      //const copy = [...countBookMark.bookmarkCount];
-      // const filter = copy.filter((item: any) => {
-      //   return item !== currentUser;
-      // });
       console.log("북마크 삭제");
       await deleteDoc(
         doc(dbService, "user", currentUser, "bookmarkPost", props.postId)
       );
-      // updateDoc(doc(dbService, "user", currentUser), {
-      //   bookmarkCount: filter,
-      // });
     } else {
       setToggleBookmark(!toggleBookmark);
       console.log("북마크 추가");
-      // const copy = [...countBookMark.bookmarkCount];
-      // copy.push(currentUser);
-      // await setDoc(doc(dbService, "user", currentUser, "bookmarkPost"), {
-      //   recipeData: props.postId,
-      // });
       await setDoc(
         doc(dbService, "user", currentUser, "bookmarkPost", props.postId),
         {
-          postId: props.postId,
+          recipeData: props.postId,
         }
       );
-      //if 배열에 uid가 있으면 추가x,없으면 추가.
-      // if (countBookMark.bookmarkCount?.includes(currentUser)) {
-      //   copy;
-      // } else {
-      //   await updateDoc(doc(dbService, "user", currentUser), {
-      //     bookmarkCount: props.postId,
-      //   });
-      //}
     }
   };
 
   return (
     <>
-      {props.userData === "geust" ? null : bookMark.findIndex(
-          (mark) => mark.id === props.postId
-        ) ? (
+      {props.userData === "geust" ? null : toggleBookmark ? (
         <button onClick={bookMarkPost}>북마크삭제</button>
       ) : (
         <button onClick={bookMarkPost}>북마크추가</button>
       )}
-
-      {/* <div>북마크 갯수:{countBookMark?.bookmarkCount.length}</div> */}
     </>
   );
 };
