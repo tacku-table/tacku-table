c9037107ef772d02cb92453ed88fc2d3f8c2217a
JH-anfseo <51734656+JH-anfseo@users.noreply.github.com>
2023-02-23 00:20:58 +0900

    feat : header 회원가입 컴포넌트 이동 추가

--- pages/loginPage/index.tsx
+++ pages/loginPage/index.tsx
@@ -2,18 +2,30 @@ import FindPassword from "@/components/auth/findPassword";
 import Login from "@/components/auth/Login";
 import Register from "@/components/auth/Register";
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
-    const [status, setStatus] = useState("login");
-    return (
+  const router = useRouter();
+  const [status, setStatus] = useState("login");
+  const { headerstatus } = router.query;
+  console.log(router.query);
+  return (
+    <div>
+      {headerstatus ? (
         <div className="grid items-center mx-auto mt-14">
-            {status === "login" && (
-                <Login setStatus={setStatus} status={status} />
-            )}
-            {status === "signUp" && <Register />}
-            {status === "searchPW" && <FindPassword />}
+          <Register />
         </div>
-    );
+      ) : (
+        <div className="grid items-center mx-auto mt-14">
+          {status === "login" && (
+            <Login setStatus={setStatus} status={status} />
+          )}
+          {status === "signUp" && <Register />}
+          {status === "searchPW" && <FindPassword />}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default LoginPage;
