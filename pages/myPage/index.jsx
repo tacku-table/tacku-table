d0fe7affdcc85e67155e77ee29f813e03ed0a1ff
harenohee <panda4859@gmail.com>
2023-02-23 16:21:39 +0900

    fix: svg 에러 확인 후 수정
    ariaHidden 에서 aria-hidden으로 변경
    
    by @harenohee

--- pages/myPage/index.jsx
+++ pages/myPage/index.jsx
@@ -78,13 +78,13 @@ const MyPage = () => {
               }}
             >
               <svg
-                className="w-9 h-9"
+                className="w-9 h-9 cursor-pointer"
                 fill="none"
                 stroke="currentColor"
                 strokeWidth="1.5"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
-                ariaHidden="true"
+                aria-hidden="true"
               >
                 <path
                   strokeLinecap="round"
