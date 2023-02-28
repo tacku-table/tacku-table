dd2bdfa1f8402fdee3afd9b0b852ea4efd8646c9
harenohee <panda4859@gmail.com>
2023-02-28 15:12:30 +0900

    fix: 회원정보 수정페이지 이미지 렌더링 이슈 해결
    - 조회페이지와 마찬가지로 새로고침이 필요했던 부분 수정
    
    #86 by @harenohee

--- pages/myPage/[...params].jsx
+++ pages/myPage/[...params].jsx
@@ -47,8 +47,12 @@ export default function ProfileEdit(props) {
 
   useEffect(() => {
     setUserInfo(props.userData);
-    getUserProfileImg();
+    if (userInfo) {
+      getUserProfileImg(userInfo?.userImg);
+    }
   }, [userInfo]);
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     const currentUser = JSON.parse(sessionStorage.getItem("User"));
@@ -90,19 +94,16 @@ export default function ProfileEdit(props) {
     }
   };
 
-  const inputChangeSetFunc = (event, setFunction) => {
-    setFunction(event.target.value);
-  };
-  const getUserProfileImg = async () => {
-    if (userInfo?.userImg === "null") {
+  const getUserProfileImg = async (userImg) => {
+    if (userImg === "null") {
       return setShowUserUpdateImg(defaultImg);
     }
     const imageListRef = ref(storage, "profileImage/");
     await listAll(imageListRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
-          if (url === userInfo?.userImg) {
-            return setShowUserUpdateImg(url);
+          if (url === userImg) {
+            setShowUserUpdateImg(url);
           }
         });
       });
@@ -229,27 +230,7 @@ export default function ProfileEdit(props) {
             <span className="text-base  min-w-[120px]">프로필 이미지</span>
             <div className="flex items-end space-x-5">
               <label className="cursor-pointer hover:opacity-40">
-                <Image
-                  src={showUserUpdateImg}
-                  className="rounded-md aspect-square"
-                  loader={({ src }) => src}
-                  priority={true}
-                  width={100}
-                  height={100}
-                  alt="프리뷰|업데이트이미지"
-                />
-
-                {/* {userInfo?.userImg === "null" ? (
-                  <Image
-                    src={defaultImg}
-                    className="rounded-md aspect-square"
-                    // loader={({ src }) => src}
-                    priority={true}
-                    width={100}
-                    height={100}
-                    alt="기본이미지"
-                  />
-                ) : (
+                {showUserUpdateImg && (
                   <Image
                     src={showUserUpdateImg}
                     className="rounded-md aspect-square"
@@ -259,7 +240,8 @@ export default function ProfileEdit(props) {
                     height={100}
                     alt="프리뷰|업데이트이미지"
                   />
-                )} */}
+                )}
+
                 <input
                   id="picture"
                   type="file"
