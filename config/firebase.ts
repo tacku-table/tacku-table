b46f78da212cd0e73752abae4e982812240ed0d2
JH-anfseo <51734656+JH-anfseo@users.noreply.github.com>
2023-03-02 00:42:17 +0900

    feat : 카카오톡 공유하기 추가, 구글로그인 추가

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// 채하
+// 재희 firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyANw6jE7NE7yF6F8TYYJalVwD2FuOLTqJ0",
-    authDomain: "taku-e9992.firebaseapp.com",
-    projectId: "taku-e9992",
-    storageBucket: "taku-e9992.appspot.com",
-    messagingSenderId: "139053130279",
-    appId: "1:139053130279:web:ae70760c52da59bb6b2402",
+  apiKey: "AIzaSyDwf81KBJ6rlKDJRts_oq2aPPDfH8LL8aQ",
+  authDomain: "test-3a207.firebaseapp.com",
+  projectId: "test-3a207",
+  storageBucket: "test-3a207.appspot.com",
+  messagingSenderId: "713080381754",
+  appId: "1:713080381754:web:3719f1abfc305e5b08b003",
 };
 
 // Initialize Firebase
@@ -18,3 +18,7 @@ export const app = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
 export const dbService = getFirestore(app);
 export const storage = getStorage(app);
+export const apiKey = firebaseConfig.apiKey;
+
+// 구글
+//export const providerGoogle = new GoogleAuthProvider();
--- config/global.d.ts
+++ config/global.d.ts
@@ -1,20 +1,22 @@
 // global.d.ts
 declare global {
-    interface RecipeProps {
-        id?: string | number;
-        uid?: string;
-        animationTitle?: string;
-        foodTitle?: string;
-        ingredient?: string;
-        cookingTime?: string;
-        foodCategory?: string;
-        displayStatus?: string;
-        thumbnail?: string;
-        createdAt?: string | number;
-        content?: string;
-        children?: JSX.Element | JSX.Element[];
-        bookmarkCount?: string[];
-    }
+  interface RecipeProps {
+    id?: string | number;
+    uid?: string;
+    animationTitle?: string;
+    foodTitle?: string;
+    ingredient?: string;
+    cookingTime?: string;
+    foodCategory?: string;
+    displayStatus?: string;
+    thumbnail?: string;
+    createdAt?: string | number;
+    content?: string;
+    children?: JSX.Element | JSX.Element[];
+    bookmarkCount?: string[];
+  }
+  interface Window {
+    Kakao: any;
+  }
 }
-
 export {};
