ecc8566afd435575e6194a4595b64633f68d32cc
harenohee <panda4859@gmail.com>
2023-03-03 00:40:27 +0900

    feat: 마이페이지 즐겨찾기 삭제 기능 추가
    -
    
    #110 by @harenohee

--- components/profile/MyTabs.jsx
+++ components/profile/MyTabs.jsx
@@ -9,12 +9,13 @@ import {
   where,
   doc,
   getDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import defaultImg from "../../public/images/test1.png";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 
 // interface MyTabsProps {
 //   userInfo: any;
@@ -99,14 +100,13 @@ const MyTabs = ({ userInfo, setUserInfo }) => {
     const q = query(collection(dbService, `user/${userId}/bookmarkPost`));
 
     onSnapshot(q, async (snapshots) => {
-      const myposts = snapshots.docs.map((d) => {
-        // console.log(d.data());
+      const myposts = snapshots.docs.map((doc) => {
         const mypost = {
-          postId: d.id,
-          writerUid: d.data().uid,
-          writerdisplayName: d.data().writerNickName,
-          writerImg: d.data().writerProfileImg,
-          ...d.data(),
+          postId: doc.id,
+          writerUid: doc.data().uid,
+          writerdisplayName: doc.data().writerNickName,
+          writerImg: doc.data().writerProfileImg,
+          ...doc.data(),
         };
         return mypost;
       });
@@ -172,6 +172,32 @@ const MyTabs = ({ userInfo, setUserInfo }) => {
       });
       setCommentPost(myposts);
     });
+  };
+  const toastAlert = (alertText) => {
+    toast(`${alertText}`, {
+      position: "top-right",
+      autoClose: 1300,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
+  const handleDeleteBookmark = async (p) => {
+    console.log(p.uid, p.postId);
+    const userConfirm = window.confirm("즐겨찾기 레시피를 삭제하시겠습니까?");
+    if (userConfirm) {
+      try {
+        await deleteDoc(doc(dbService, `user/${p.uid}/bookmarkPost`, p.postId));
+        getMyBookmark(userInfo.userId);
+        toastAlert("🗑 삭제되었습니다");
+      } catch (error) {
+        toast.error("삭제에 실패하였습니다. 다시 시도해주세요.", error);
+      }
+    }
   };
 
   return (
@@ -245,7 +271,7 @@ const MyTabs = ({ userInfo, setUserInfo }) => {
                   </Link>
                 </div>
               </div>
-              <div className="flex mt-9 ml-8 space-x-3">
+              <div className="flex mt-9 ml-8 space-x-3 relative items-center">
                 {p.writerImg === "null" ? (
                   <Image
                     className="aspect-square rounded-md object-cover w-12 h-12"
@@ -268,6 +294,24 @@ const MyTabs = ({ userInfo, setUserInfo }) => {
                   />
                 )}
                 <p className="text-[16px]">{p.writerdisplayName}</p>
+                <svg
+                  className="w-6 h-6 absolute right-8 cursor-pointer hover:text-brand100"
+                  onClick={() => {
+                    handleDeleteBookmark(p);
+                  }}
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  ></path>
+                </svg>
               </div>
             </div>
           ))}
