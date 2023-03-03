bd020794dda34ec2e8eec6e6ff72d64d429b44d6
hongdakyung <bongbk72@naver.com>
2023-03-03 12:21:58 +0900

    release version.1.1

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-        styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
-    },
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // Next에게 styled-component도 처리해달라고 옵션을 설정해줌
+  },
 };
 
 module.exports = nextConfig;
@@ -22,5 +22,26 @@ module.exports = withVideos();
 module.exports = withSentryConfig(
   module.exports,
   { silent: true },
-  { hideSourcemaps: true },
+  { hideSourcemaps: true }
 );
+
+const moduleExports = {
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
+};
+
+module.exports = withSentryConfig(moduleExports);
+
+// const moduleExports = {
+//   sentry: {
+//     hideSourceMaps: true,
+//   },
+// };
+
+//  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+
+// module.exports = withSentryConfig(moduleExports, {
+//   dryRun: process.env.VERCEL_ENV !== "production",
+// });
