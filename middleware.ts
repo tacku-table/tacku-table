6cd6adbe09fa52afc89a4845708f647086e94a48
hongdakyung <bongbk72@naver.com>
2023-03-03 15:53:34 +0900

    !HotFix
    - middleware 코드 수정

--- middleware.ts
+++ middleware.ts
@@ -6,28 +6,20 @@ import { dbService } from "./config/firebase";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+  // 두번 실행되고 두번째때 undefined가 뜸
   const nextURL = request.nextUrl.pathname;
-  console.log("nextURL:", nextURL);
   const splitURL = nextURL.split("/");
-  console.log("splitURL:", splitURL);
   const id = splitURL[2];
-  console.log("id:", id);
 
   const snap = await getDoc(doc(dbService, "recipe", id));
   if (snap.exists()) {
     return;
   } else {
-    return NextResponse.redirect(
-      new URL(
-        "https://tacku-table-v1-flludcxgy-tacku-table.vercel.app/detailRecipePage",
-        request.url
-      )
-    );
+    return NextResponse.redirect(new URL("/deletePage", request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher:
-    "https://tacku-table-v1-flludcxgy-tacku-table.vercel.app/detailRecipePage/:path*",
+  matcher: "/detailRecipePage/:path*",
 };
