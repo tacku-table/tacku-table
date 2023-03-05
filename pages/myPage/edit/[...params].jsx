a8aec6ce1ba1ebd9187725c661986fcbd07f9925
harenohee <panda4859@gmail.com>
2023-03-06 02:27:40 +0900

    refactor: 프로필이미지 업로드 ui 변경
    - 이미지편집, 저장하기 버튼 생성
    
    #124 by @harenohee

--- pages/myPage/edit/[...params].jsx
+++ pages/myPage/edit/[...params].jsx
@@ -236,7 +236,7 @@ export default function ProfileEdit(props) {
           userImg: url,
         }).then(() => {
           setImgPreview("uploading");
-          console.log("컬렉션 업데이트 성공!");
+          toastAlert("프로필 이미지 변경 완료");
         });
         // setImgPreview(url);
         setShowUserUpdateImg(url);
@@ -251,68 +251,42 @@ export default function ProfileEdit(props) {
         <div className="flex flex-col min-w-[532px] py-10 space-y-7">
           <div className="flex gap-14 items-start">
             <span className="text-base  min-w-[120px]">프로필 이미지</span>
-            <div className="flex items-end space-x-5">
-              <label className="cursor-pointer hover:opacity-40">
-                {showUserUpdateImg && (
+            <div className="flex flex-col">
+              {showUserUpdateImg && (
+                <div>
+                  <Image
+                    src={showUserUpdateImg}
+                    className="rounded-md aspect-square"
+                    loader={({ src }) => src}
+                    priority={true}
+                    width={150}
+                    height={150}
+                    alt="프리뷰|업데이트이미지"
+                  />
+                </div>
+              )}
+              <div className="mt-3 flex justify-between space-x-2">
+                <label className="cursor-pointer">
+                  <div className=" text-white disabled:opacity-50 bg-brand100 hover:bg-brand100 focus:ring-4 focus:outline-none focus:ring-brand100/50 font-medium rounded-sm text-sm px-2 py-2 text-center inline-flex justify-center items-center dark:hover:bg-brand100/80 dark:focus:ring-brand100/40 ">
+                    <span>이미지 편집</span>
+                  </div>
+                  <input
+                    id="picture"
+                    type="file"
+                    accept="image/*"
+                    hidden
+                    onChange={handleImageFile}
+                  />
+                </label>
+
+                {imageUpload && (
                   <div
                     onClick={() => handleUpdateProfile(userInfo.userId)}
                     disabled={!imageUpload}
+                    className="cursor-pointer text-white disabled:opacity-50 bg-brand100 hover:bg-brand100 focus:ring-4 focus:outline-none focus:ring-brand100/50 font-medium rounded-sm text-sm px-2 py-2 text-center inline-flex justify-center dark:hover:bg-brand100/80 dark:focus:ring-brand100/40 "
                   >
-                    <Image
-                      src={showUserUpdateImg}
-                      className="rounded-md aspect-square"
-                      loader={({ src }) => src}
-                      priority={true}
-                      width={100}
-                      height={100}
-                      alt="프리뷰|업데이트이미지"
-                    />
-                    <div className="relative">
-                      <svg
-                        className="absolute bottom-2 right-2 w-6 h-6 text-black"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
-                        ></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
-                        ></path>
-                      </svg>
-                    </div>
+                    <span>저장하기</span>
                   </div>
-                )}
-
-                <input
-                  id="picture"
-                  type="file"
-                  accept="image/*"
-                  hidden
-                  onChange={handleImageFile}
-                />
-              </label>
-              <div className="flex flex-col items-start">
-                <button
-                  onClick={() => handleUpdateProfile(userInfo.userId)}
-                  type="button"
-                  disabled={!imageUpload}
-                  className="text-white disabled:opacity-50 bg-brand100 hover:bg-brand100 focus:ring-4 focus:outline-none focus:ring-brand100/50 font-medium rounded-sm text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-brand100/80 dark:focus:ring-brand100/40 "
-                >
-                  수정
-                </button>
-                {imgPreview === "uploading" && (
-                  <span className="text-sm text-blue100">
-                    프로필이미지 변경 완료
-                  </span>
                 )}
               </div>
             </div>
