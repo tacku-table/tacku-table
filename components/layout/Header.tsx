73af4201dabb78d2ca56558eeeba65d2a4a4fc33
hongdakyung <bongbk72@naver.com>
2023-03-06 20:23:13 +0900

    refactor
    - 리드미 홈페이지 링크 수정
    - 회원가입 페이지 toast.promise 사용하여 로딩중 로직 추가
    - Footer에 yarn version 추가
    - 레이아웃의 <div>태그 </> 태그로 변경
    - Header의 ts 리팩토링
    
    by @ddoqi

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,136 +11,131 @@ import { setTimeout } from "timers";
 import { useRouter } from "next/router";
 
 export const clearStorage = () => {
-    sessionStorage.removeItem("filteredFoodData");
-    sessionStorage.removeItem("filteredTimeData");
-    sessionStorage.removeItem("searchData");
-    sessionStorage.removeItem("userWatching");
+  sessionStorage.removeItem("filteredFoodData");
+  sessionStorage.removeItem("filteredTimeData");
+  sessionStorage.removeItem("searchData");
+  sessionStorage.removeItem("userWatching");
 };
 
 const Header = () => {
-    const [storageCurrentUser, setStorageCurrentUser] = useState("");
-    const toastAlert = (alertText: string) => {
-        toast(`${alertText}`, {
-            position: "top-right",
-            autoClose: 1300,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-        });
-    };
-    const logoutAction = () => {
-        signOut(authService)
-            .then(() => {
-                sessionStorage.clear();
-                toastAlert("로그아웃 성공!");
-                setTimeout(() => {
-                    location.reload();
-                }, 2000);
-            })
-            .catch((error) => {
-                console.log("error:", error);
-            });
-    };
-    const moveLoginPage = () => {
-        location.href = "/login";
-    };
-    // 마이페이지
-    const router = useRouter();
-    const moveMyPage = (currentUser: any) => {
-        const { uid } = JSON.parse(currentUser);
-        location.href = `/profile/${uid}`;
-    };
+  const [storageCurrentUser, setStorageCurrentUser] = useState("");
+  const toastAlert = (alertText: string) => {
+    toast(`${alertText}`, {
+      position: "top-right",
+      autoClose: 1300,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+  const logoutAction = () => {
+    signOut(authService)
+      .then(() => {
+        sessionStorage.clear();
+        toastAlert("로그아웃 성공!");
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log("error:", error);
+      });
+  };
+  const moveLoginPage = () => {
+    location.href = "/login";
+  };
+  // 마이페이지
+  const router = useRouter();
+  const moveMyPage = (currentUser: string) => {
+    const { uid } = JSON.parse(currentUser);
+    location.href = `/profile/${uid}`;
+  };
 
-    const clearStorageAndShowTotal = () => {
-        sessionStorage.removeItem("filteredFoodData");
-        sessionStorage.removeItem("filteredTimeData");
-        sessionStorage.removeItem("searchData");
-        window.location.replace("/search");
-    };
+  const clearStorageAndShowTotal = () => {
+    sessionStorage.removeItem("filteredFoodData");
+    sessionStorage.removeItem("filteredTimeData");
+    sessionStorage.removeItem("searchData");
+    window.location.replace("/search");
+  };
 
-    useEffect(() => {
-        const user = sessionStorage.getItem("User") || "";
-        setStorageCurrentUser(user);
-    }, []);
+  useEffect(() => {
+    const user = sessionStorage.getItem("User") || "";
+    setStorageCurrentUser(user);
+  }, []);
 
-    return (
-        <div
-            className="fixed top-0 z-50 w-full h-20 px-24 py-7 bg-white border-b-[1.5px] border-mono50 flex justify-between items-center text-sm"
-            onClick={clearStorage}
+  return (
+    <div
+      className="fixed top-0 z-50 w-full h-20 px-24 py-7 bg-white border-b-[1.5px] border-mono50 flex justify-between items-center text-sm"
+      onClick={clearStorage}
+    >
+      <ul className="flex justify-center items-center">
+        <li className="header-title">
+          <Link href="/main">
+            <Image src={logo2} alt="logo_web" width={155} height={38} />
+          </Link>
+        </li>
+        <li
+          onClick={clearStorageAndShowTotal}
+          className="header-title cursor-pointer"
         >
-            <ul className="flex justify-center items-center">
-                <li className="header-title">
-                    <Link href="/main">
-                        <Image
-                            src={logo2}
-                            alt="logo_web"
-                            width={155}
-                            height={38}
-                        />
-                    </Link>
-                </li>
-                <li
-                    onClick={clearStorageAndShowTotal}
-                    className="header-title cursor-pointer"
-                >
-                    전체 레시피
-                </li>
-                <FoodCategory />
-                <CookingTime />
-                <li>
-                    <Link href="/community" className="header-title">
-                        커뮤니티
-                    </Link>
-                </li>
-            </ul>
-            <div className="flex justify-center items-center">
-                <Link
-                    href="/recipeWrite"
-                    className="mr-6 hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
-                >
-                    레시피글쓰기
-                </Link>
-                {storageCurrentUser ? (
-                    <button
-                        type="button"
-                        onClick={logoutAction}
-                        className="mr-6 hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
-                    >
-                        로그아웃
-                    </button>
-                ) : (
-                    <button
-                        type="button"
-                        onClick={moveLoginPage}
-                        className="sorted-btn mr-2"
-                    >
-                        로그인
-                    </button>
-                )}
-                {storageCurrentUser ? (
-                    <button
-                        onClick={() => moveMyPage(storageCurrentUser)}
-                        className="hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
-                    >
-                        마이페이지
-                    </button>
-                ) : (
-                    <Link
-                        href={{
-                            pathname: "/login",
-                            query: { headerstatus: "headerstatus" }, // array라 문자화
-                        }}
-                        className="sorted-btn"
-                    >
-                        회원가입
-                    </Link>
-                )}
-            </div>
-        </div>
-    );
+          전체 레시피
+        </li>
+        <FoodCategory />
+        <CookingTime />
+        <li>
+          <Link href="/community" className="header-title">
+            커뮤니티
+          </Link>
+        </li>
+      </ul>
+      <div className="flex justify-center items-center">
+        <Link
+          href="/recipeWrite"
+          className="mr-6 hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
+        >
+          레시피글쓰기
+        </Link>
+        {storageCurrentUser ? (
+          <button
+            type="button"
+            onClick={logoutAction}
+            className="mr-6 hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
+          >
+            로그아웃
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={moveLoginPage}
+            className="sorted-btn mr-2"
+          >
+            로그인
+          </button>
+        )}
+        {storageCurrentUser ? (
+          <button
+            onClick={() => moveMyPage(storageCurrentUser)}
+            className="hover:text-mono80 hover:transition hover:ease-out hover:duration-300"
+          >
+            마이페이지
+          </button>
+        ) : (
+          <Link
+            href={{
+              pathname: "/login",
+              query: { headerstatus: "headerstatus" }, // array라 문자화
+            }}
+            className="sorted-btn"
+          >
+            회원가입
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,11 +2,11 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout({ children }: { children: any }) {
-    return (
-        <div>
-            <Header />
-            <div className="pt-[83px] min-h-screen">{children}</div>
-            <Footer />
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <div className="pt-[83px] min-h-screen">{children}</div>
+      <Footer />
+    </>
+  );
 }
