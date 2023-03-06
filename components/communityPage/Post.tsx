846d16c04752a9fc38cca4709aed9f171ef1413f
harenohee <panda4859@gmail.com>
2023-03-06 14:08:04 +0900

    fix: 날짜 텍스트가 늘어남에 따른 레이아웃 시프트 해결
    - width 속성 추가
    
    #129 by @harenohee

--- components/communityPage/Post.tsx
+++ components/communityPage/Post.tsx
@@ -5,66 +5,62 @@ import defaultImg from "../../public/images/test1.png";
 import useGetUserProfileNickName from "@/hooks/useGetUserProfileNickName";
 
 const Post = ({ p }: any) => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const writterUid = p.writterUid;
+  const writterUid = p.writterUid;
 
-    const { userNickName: writterNickname } =
-        useGetUserProfileNickName(writterUid);
+  const { userNickName: writterNickname } =
+    useGetUserProfileNickName(writterUid);
 
-    useEffect(() => {
-        if (writterNickname) {
-            setIsLoading(false);
-        }
-    });
-    if (isLoading) {
-        return <></>;
+  useEffect(() => {
+    if (writterNickname) {
+      setIsLoading(false);
     }
+  });
+  if (isLoading) {
+    return <></>;
+  }
 
-    return (
-        <div className="border-b border-mono60 py-4 px-5 flex text-sm">
-            <Link legacyBehavior href={`/community/${p.id}`}>
-                {p.thumbnail === "" ? (
-                    <Image
-                        className="object-cover aspect-[4/3] rounded-md cursor-pointer"
-                        src={defaultImg}
-                        priority={true}
-                        width={70}
-                        height={41}
-                        alt="community-thumbnail-default"
-                    />
-                ) : (
-                    <Image
-                        className="object-cover aspect-[4/3] rounded-md cursor-pointer"
-                        src={p.thumbnail}
-                        priority={true}
-                        loader={({ src }) => src}
-                        width={70}
-                        height={41}
-                        alt="community-thumbnail"
-                    />
-                )}
-            </Link>
-            <div className="pl-5">
-                <Link legacyBehavior href={`/community/${p.id}`}>
-                    <a>{p.title}</a>
-                </Link>
-                <div className="flex mt-3 text-mono70">
-                    <div className="border-r border-mono60 pr-3">
-                        {p.category}
-                    </div>
-                    <div className="border-r border-mono60 px-3">
-                        {p.writtenDate}
-                    </div>
-                    <Link legacyBehavior href={`/profile/${p.writterUid}`}>
-                        <span className="pl-3 cursor-pointer">
-                            {writterNickname}
-                        </span>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="border-b border-mono60 py-4 px-5 flex text-sm">
+      <Link legacyBehavior href={`/community/${p.id}`}>
+        {p.thumbnail === "" ? (
+          <Image
+            className="object-cover aspect-[4/3] rounded-md cursor-pointer"
+            src={defaultImg}
+            priority={true}
+            width={70}
+            height={41}
+            alt="community-thumbnail-default"
+          />
+        ) : (
+          <Image
+            className="object-cover aspect-[4/3] rounded-md cursor-pointer"
+            src={p.thumbnail}
+            priority={true}
+            loader={({ src }) => src}
+            width={70}
+            height={41}
+            alt="community-thumbnail"
+          />
+        )}
+      </Link>
+      <div className="pl-5">
+        <Link legacyBehavior href={`/community/${p.id}`}>
+          <a>{p.title}</a>
+        </Link>
+        <div className="flex mt-3 text-mono70">
+          <div className="border-r border-mono60 pr-3">{p.category}</div>
+          <div className="border-r w-[147px]  border-mono60 px-3">
+            {p.writtenDate}
+          </div>
+          <Link legacyBehavior href={`/profile/${p.writterUid}`}>
+            <span className="pl-3 cursor-pointer">{writterNickname}</span>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Post;
