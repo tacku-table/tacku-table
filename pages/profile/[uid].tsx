0b2a663eec8a7cb8c6915f856e197569275e04d3
harenohee <panda4859@gmail.com>
2023-03-09 20:40:54 +0900

    design: 회원정보 조회 및 수정페이지 반응형 ui 개선
    -
    
    @175 by @harenohee

--- pages/profile/[uid].tsx
+++ pages/profile/[uid].tsx
@@ -65,13 +65,14 @@ const MyPage = () => {
   }
 
   return (
-    <div className="min-w-full mx-auto my-0 justify-center">
-      <div className="bg-coverBg bg-cover bg-center h-[250px] bg-no-repeat relative">
-        <div className="flex justify-center items-center space-x-8 absolute left-[300px] top-[120px] xl:left-[380px] text-white">
+    <div className="mx-auto my-0 sm:w-full">
+      <div className="bg-coverBg bg-cover bg-center h-[200px] sm:h-[250px] bg-no-repeat relative">
+        <div className="flex justify-center items-center space-x-4 sm:space-x-8 absolute sm:left-[28%] left-[20%] sm:top-[120px] top-1/3 text-white">
+          {/* <div className="flex justify-center items-center space-x-8 absolute left-[50px] top-1/3 sm:left-[300px] sm:top-[120px] xl:left-[380px] text-white"> */}
           {imgPreview && (
             <Image
               src={imgPreview}
-              className="w-[100px] h-[100px] rounded-md aspect-square"
+              className="w-[100px] h-[100px] rounded-md aspect-square  mx-auto sm:mx-0"
               loader={({ src }) => src}
               unoptimized
               priority={true}
@@ -80,41 +81,44 @@ const MyPage = () => {
               alt="프로필이미지"
             />
           )}
-          <p className="text-4xl font-semibold">{userInfo?.userNickname}</p>
-          {storageCurrentUser?.uid === userInfo?.userId && (
-            <Link
-              legacyBehavior
-              href={{
-                pathname: `/profile/edit/${userInfo?.userNickname}`,
-                query: {
-                  id: userInfo?.userId,
-                  userImg: userInfo?.userImg,
-                },
-              }}
-              as={`/profile/edit/${userInfo?.userNickname}`}
-            >
-              <svg
-                className="w-9 h-9 cursor-pointer"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
+          <div className="md:flex-none flex md:mt-0 mt-4 space-x-2">
+            <p className="sm:text-4xl text-3xl font-semibold">
+              {userInfo?.userNickname}
+            </p>
+            {storageCurrentUser?.uid === userInfo?.userId && (
+              <Link
+                legacyBehavior
+                href={{
+                  pathname: `/profile/edit/${userInfo?.userId}`,
+                  query: {
+                    id: userInfo?.userId,
+                    userImg: userInfo?.userImg,
+                  },
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-                ></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                ></path>
-              </svg>
-            </Link>
-          )}
+                <svg
+                  className="w-9 h-9 cursor-pointer"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  ></path>
+                </svg>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
 
