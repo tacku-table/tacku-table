2741c243b3c636510815fe35848f70241c71e32f
harenohee <panda4859@gmail.com>
2023-03-09 21:40:46 +0900

    design: 마이페이지 탭 반응형 적용
    -
    
    #175 by @harenohee

--- components/profilePage/MyCommunityTab.tsx
+++ components/profilePage/MyCommunityTab.tsx
@@ -16,20 +16,22 @@ const MyCommunityTab = ({ userInfo }: MyTabProp) => {
   }, [communityPost]);
 
   return (
-    <Tab.Panel className="pb-6">
+    <Tab.Panel className="pb-6 w-full">
       {communityList.length === 0 && <EmptyPost />}
       {communityList?.map((p) => (
-        <div key={p.id} className="p-6">
+        <div key={p.id} className="pt-6 px-1 sm:p-6">
           <hr className="border-mono50 mx-8 mb-6 border-[1px]" />
-          <div className="pl-8 space-x-[20px] items-center flex">
-            <div className="flex flex-col">
-              <div className="space-x-[10px]">
+          <div className="pl-8 space-x-4 sm:space-x-[20px] items-center flex">
+            <div className="flex flex-col space-y-3">
+              <div className="sm:space-x-[10px] flex space-x-1 w-full text-xs sm:text-base text-mono80">
                 <span>{p.category}게시판</span>
                 <span>|</span>
                 <span>{p.writtenDate}</span>
               </div>
               <Link legacyBehavior href={`/community/${p.id}`}>
-                <a className="text-2xl font-semibold">{p.title}</a>
+                <a className="text-lg sm:text-[24px] font-semibold text-mono100">
+                  {p.title}
+                </a>
               </Link>
             </div>
           </div>
