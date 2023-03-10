5dcbad5349107689ca585ca775dfd141c9f9aba8
be-brain <chha5232@gmail.com>
2023-03-10 17:46:27 +0900

    fix : footer 모바일크기수정

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,19 +6,19 @@ const Footer = () => {
     const { version } = packageJson;
 
     return (
-        <div className="bg-[#EBD8C3] w-full flex flex-col justify-start text-mono100 mx-auto">
-            <ul className="border-b border-white pt-6 w-4/6 mx-auto">
+        <div className="bg-[#EBD8C3] w-full flex flex-col justify-start text-mono100 mx-auto  text-xs">
+            <ul className="border-b border-white pt-3 md:pt-6 w-4/6 mx-auto">
                 <li className="mb-4">
-                    <span className="mr-7 text-xs">고객센터</span>
-                    <span className="text-base font-semibold">
+                    <span className="mr-7">고객센터</span>
+                    <span className="text-sm font-medium md:font-semibold">
                         takus@email.com
                     </span>
                 </li>
-                <li className="mb-4 text-xs">
+                <li className="mb-4 hidden md:block">
                     copyright © 타쿠의 식당. All rights reserved.
                 </li>
             </ul>
-            <ul className="bottom-0 pt-4 pb-10 flex gap-7 text-xs w-4/6 mx-auto justify-between">
+            <ul className="bottom-0 pt-4 pb-5 md:pb-10 flex gap-7 w-4/6 mx-auto justify-between">
                 <div className="space-y-1 md:flex md:items-center md:space-y-0 md:gap-x-10 xl:gap-x-16">
                     <li>
                         <Link href="/intro">사이트소개</Link>
