e05f674e9a7c624628e4df3790f94288d09f77cd
harenohee <panda4859@gmail.com>
2023-03-10 19:43:55 +0900

    design: 빈게시글 ui 수정
    -
    
    #175 by @harenohee

--- components/profilePage/EmptyPost.tsx
+++ components/profilePage/EmptyPost.tsx
@@ -6,16 +6,16 @@ const EmptyPost = () => {
     <div className="flex flex-col my-auto items-center">
       <div className="text-center m-auto pt-[183px] pb-[184px]">
         <Image
-          className="m-auto w-[240px] h-[181px] object-cover object-center"
+          className="m-auto sm:w-[240px] sm:h-[181px] object-cover object-center w-2/1"
           src={emptyPostImg}
           width={300}
           height={300}
           alt="삭제된 페이지입니다."
         />
 
-        <h3 className="text-[24px] font-semibold mt-[29px]">
+        <span className="sm:text-2xl text-sm font-medium sm:mt-9 mt-4">
           게시글이 존재하지 않습니다.
-        </h3>
+        </span>
       </div>
     </div>
   );
