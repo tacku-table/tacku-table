6fe839222ea8cc93f98dee5ee6f01b809d33d2b6
be-brain <chha5232@gmail.com>
2023-03-11 10:55:51 +0900

    feat : next.videos제거, 코드정리

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -36,7 +36,6 @@ const RegisterPage = () => {
         register,
         handleSubmit,
         getValues,
-        watch,
         formState: { errors },
     } = useForm<RegisterForm>({ mode: "onChange" });
     const onValid = (data: RegisterForm) => {
@@ -47,7 +46,6 @@ const RegisterPage = () => {
     };
     const [showPw, setShowPw] = useState(false);
     const [showPwConfirm, setShowPwConfirm] = useState(false);
-    const [isUsing, setIsUsing] = useState([]);
     const [nicknameCheck, setNicknameCheck] = useState(false);
     const [notNicknameDuplicateCheck, setNotNicknameDuplicateCheck] =
         useState(true);
