d8e8fa7165d5215cf5379bc78df5d770e9cea0df
be-brain <chha5232@gmail.com>
2023-03-16 20:20:43 +0900

    feat : firebase변경

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,13 +4,21 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 //배포용
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_APP_ID,
+// };
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+    apiKey: "AIzaSyANw6jE7NE7yF6F8TYYJalVwD2FuOLTqJ0",
+    authDomain: "taku-e9992.firebaseapp.com",
+    projectId: "taku-e9992",
+    storageBucket: "taku-e9992.appspot.com",
+    messagingSenderId: "139053130279",
+    appId: "1:139053130279:web:ae70760c52da59bb6b2402",
 };
 
 let firebase;
